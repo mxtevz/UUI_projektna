0c27a3ca6e0f37799ffeeb36a9a26d6699eebb3e
jedivrane <matevz.prasnikar@dijak.stps-tr.si>
2022-12-15 17:19:58 +0100

    furi

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 import numpy as np
-data = open("clustered.csv")
+data = open("UUI_projektna\clustered.csv")
 csvreader = csv.reader(data)
 header = []
 header = next(csvreader)
@@ -10,9 +10,25 @@ for row in csvreader:
    rows.append(row)
 
 rows_np = np.array(rows)
-avg_r = 0
-avg_g = 0
-avg_b = 0
 
-print(rows_np)
+def average_rgb(cluster, array):
+   cluster_array = []
+   for row in array:
+      if(row[-1] == cluster):
+         cluster_array.append(row)
+   cluster_array = np.array(cluster_array)
+   cluster_array = np.delete(cluster_array, 3, axis=1)
+   cluster_array = cluster_array.astype(np.float64)
+   return cluster_array.mean(axis=0).astype(int)
+
+print(average_rgb("C1", rows))
+print(average_rgb("C2", rows))
+print(average_rgb("C3", rows))
+print(average_rgb("C4", rows))
+print(average_rgb("C5", rows))
+print(average_rgb("C6", rows))
+print(average_rgb("C7", rows))
+print(average_rgb("C8", rows))
+print(average_rgb("C9", rows))
+print(average_rgb("C10", rows))
 data.close()
